e3a6805b6433b8a46e5940d9b775aa2665701a77
Michal Dupkala <mdupdevelop@gmail.com>
2021-05-05 16:16:43 +0200

    project preparation

--- src/axios-firebase.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://testwebapp-3ab8b-default-rtdb.europe-west1.firebasedatabase.app'
-})
-
-
-export default instance;
