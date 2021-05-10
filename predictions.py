1c32028fdfebfc7af3a3f4dc14e21b5787b65eab
Jáchym Dvořák <jachym.dvorak@seznam.cz>
2021-05-10 15:27:56 +0200

    Added better size parser
    Now recognizes size based on propHigh and propLow

--- predictions.py
+++ predictions.py
@@ -10,16 +10,17 @@ import pickle
 import pandas as pd
 import numpy as np
 import argparse
+import re
 
-CREATE_JSON = True
+CREATE_TEST_JSON = False
 
-if CREATE_JSON:
+if CREATE_TEST_JSON:
     test_json = {'tracker_1': 
                  { 'city': 'Praha',
                   'district': 'Praha 3',
                   'email': 'dummy@dummy.praha3',
                   'name': 'Malé byty',
-                  'propHigh': '1+1',
+                  'propHigh': '2+1',
                   'propLow': '1+kk',
                   'schedule': 1
                   }, 'tracker_2': 
@@ -36,7 +37,7 @@ if CREATE_JSON:
                   'email': 'dummy@dummy.praha1',
                   'name': 'Střední byty',
                   'propHigh': '2+1',
-                  'propLow': '2+kk',
+                  'propLow': '3+kk',
                   'schedule': 1
                   }
                  }
@@ -44,9 +45,9 @@ if CREATE_JSON:
     with open('test_json.json', 'w') as f:
         json.dump(test_json, f)
 
-CMD_LINE = True
+DEBUG = True
 
-if CMD_LINE:
+if DEBUG is not True:
 # Create the parser
     my_parser = argparse.ArgumentParser(description='Please list files to use')
     
@@ -77,6 +78,11 @@ if CMD_LINE:
      
 # LOAD DATA
 
+if DEBUG:
+    trackers = 'test_json.json'
+    models = 'all_models_dict_v2.pickle'
+    database = 'processed_dataset.csv'
+
 with open(trackers, 'r') as f:
     trackers = json.load(f)
     
@@ -87,6 +93,15 @@ df = pd.read_csv(database, index_col = 0)
 
 
 def predict(model_dict, model_type, data):
+    
+    '''
+    predicts price of a single flat based on trained model
+    
+    model_dict = loaded dictionary of pretrained models for each flat size small/medium/large
+    model_type = what model to use? small/medium/large
+    data = row of features to predict price for (a single flat)
+    
+    '''
     return ((0.35 * model_dict[model_type]['stacker'].predict(data)) +
             (0.25 * model_dict[model_type]['XGBRegressor'].predict(data)) +
             (0.15 * model_dict[model_type]['BaggingRegressor'].predict(data)) +  
@@ -94,7 +109,15 @@ def predict(model_dict, model_type, data):
     
 # PREDICT PRICES
 
-def extract_size_string(row):
+def extract_room_size_as_string(row):
+    
+    '''
+    returns a string of 'low', 'medium' or 'large' based on amount of rooms of a flat 
+    is meant to be used as df.apply(<this func>)
+    
+    row = row of dataframe
+    
+    '''
     
     if '2' in str(row):
         row = 'medium'
@@ -106,9 +129,18 @@ def extract_size_string(row):
         row = pd.NA
     return row
 
-def predict_prices(df, model_dict):
+def predict_prices_for_all_flats(df, model_dict):
+    
+    '''
+    predicts prices for all flats in specified file based on trained dictionary of models (a blender of 4 models,
+                                                                                           for each size (small, medium, large))
+    
+    df = scraped dataset of all flats, preprocessed
+    model_dict = dictionary with trained models (keys for small, medium and large model blenders)
+    
+    '''
 
-    df['rooms_string'] = df['rooms'].apply(extract_size_string)
+    df['rooms_string'] = df['rooms'].apply(extract_room_size_as_string)
     df = df.dropna(subset = ['rooms_string'])    
 
     predictions = []
@@ -135,20 +167,47 @@ def predict_prices(df, model_dict):
     
     return df_with_predictions
 
-def return_size(dictionary):
+def return_size_from_input_json(dictionary):
+    
+    '''
+    returns size required by customer specified in tracker (based on high and low interval)
+    
+    dictionary = dictionary for each tracker (works based on looping through json with tracker IDs)
+    
+    '''
 
-    if 'malé' in dictionary['name'].lower():
-        size = 'small'
-    elif 'střední' in dictionary['name'].lower():
-        size = 'medium'
-    else:
-        size = 'large'
-
+    low = re.search(r'\d+', trackers['tracker_1']['propLow'])
+    low = int(low.group(0) if low is not None else None)
+    
+    high = re.search(r'\d+', trackers['tracker_1']['propHigh'])
+    high = int(high.group(0) if high is not None else None)
+    
+    if high == low:
+        if high == 1:
+            size = 'small'
+        elif high == 2:
+            size = 'medium'
+        else:
+            size = 'large'
+            
+    elif high > 2 and low > 1:
+        size = ['medium' ,'large']
+    
+    elif high == 2:
+        size = ['small', 'medium']
+        
     return size
 
-def get_underpriced_flats(df):
+def filter_underpriced_flats(df):
     
-    underpriced_df = predict_prices(df, model_dict)
+    '''
+    filters all flats and returns only the underpriced ones
+    
+    df = dataset of all scraped flats
+    
+    '''
+    
+    underpriced_df = predict_prices_for_all_flats(df, model_dict)
     
     underpriced_df = underpriced_df[underpriced_df['price'] < underpriced_df['predicted']]
     
@@ -156,32 +215,38 @@ def get_underpriced_flats(df):
     
     return underpriced_df
 
-def recommendation_generator(dataset, request, number_of_flats): 
+def generate_recommendations_for_each_tracker(dataset, request, number_of_flats = 10):
+    
+    '''
+    generates recommendations for each tracker id based on conditions set in that tracker
+    
+    dataset = file with all scraped flats to be filtered and predicted
+    request = json file with tracker IDs and conditions 
+    number_of_flats = how many flats to recommend (default 10)
+    
+    '''
     
     recommendations = {}
     
-    underpriced_flats = get_underpriced_flats(dataset)
+    underpriced_flats = filter_underpriced_flats(dataset)
     
     for tracker, data in request.items():
     
         tracker_id = tracker
-        size = return_size(data)
+        size = return_size_from_input_json(data)
         district = data['district']
         email = data['email']
         
-        flats_temp = underpriced_flats[underpriced_flats['rooms_string'].eq(size) & \
+        flats_temp = underpriced_flats[underpriced_flats['rooms_string'].isin(size) & \
                                 underpriced_flats['district'].eq(district)].sort_values('difference', ascending = False)
+            
         recommendations[tracker_id] = {'links': list(flats_temp[:number_of_flats].index),
                                        'email': email,
                                        'district': district}
     
     return recommendations
 
-recommendations = recommendation_generator(df, trackers, 10)
+recommendations = generate_recommendations_for_each_tracker(df, trackers, 10)
 
-for k, v in recommendations.items():
-    
-    print(k, '\n\n')
-    for link in v['links']:
-        print(link, '\n')
-
+with open('recommendations.json', 'w') as out:
+    json.dump(recommendations, out)
