fbda4022d2e80fadfb677e3407ad9006dcd6e6f1
ToVic <61870120+ToVic@users.noreply.github.com>
2021-05-15 14:19:16 +0200

    intro call to action

--- src/Components/navbar_left/Intro.js
+++ src/Components/navbar_left/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Intro.css';
+import {NavLink} from 'react-router-dom';
 //import Product from './Product';
 
 const intro = (props) => {
@@ -7,6 +8,7 @@ const intro = (props) => {
         <div className="Intro">
             <h1>Kupte perspektivní nemovitosti. Výhodně.</h1>
             <p>Najdeme je za Vás.</p>
+            <div className="More"><NavLink to='/product' style={{margin: 'auto'}} exact><h2>Chci zjistit více</h2></NavLink></div>
         </div>
     )
 }
