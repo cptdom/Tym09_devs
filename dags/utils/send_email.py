182b99f4be5fdce084da0e5089da27f1f4c63a5c
Jáchym Dvořák <jachym.dvorak@seznam.cz>
2021-06-21 09:06:58 +0200

    Added table html to emails

--- dags/utils/send_email.py
+++ dags/utils/send_email.py
@@ -55,17 +55,113 @@ def send_email_with_recommendations(receiver_email, tracker_id, district, links)
 
     # write body
     html = f"""<html>
+    <meta charset="UTF-8"> 
+    <meta content="width=device-width, initial-scale=1" name="viewport"> 
+    <meta name="x-apple-disable-message-reformatting"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta content="telephone=no" name="format-detection"> 
+    <body style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"> 
+        <div class="es-wrapper-color" style="background-color:#9FC5E8"> 
                 <body>
-                    <p>Dobrý den,</p><br>
-                    <p>na základě vašeho hlídacího psa <b>{tracker_id}</b> vám posíláme následující byty ve vámi zadané lokalitě 
-                    <b>{district}</b>, které <b>{today}</b> náš systém vyhodnotil jako pravděpodobně podceněné.</p>
-                    <ul>
-                       {links_joined}
-                    </ul>
-                    <p>Hezký zbytek dne přeje,</p><br>
-                    <p>RealQuik a.s.</p>
-                    <p><img src="cid:realquik_logo"</p>
+            <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;background-color:#9FC5E8"> 
+                <tr style="border-collapse:collapse"> 
+                    <td valign="top" style="padding:0;Margin:0"> 
+                    <table class="es-content-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;width:600px" cellspacing="0" cellpadding="0" align="center" bgcolor="#333333"> 
+                        <tr style="border-collapse:collapse"> 
+                         <td align="left" style="padding:10px;Margin:0"> 
+                            <table class="es-left" cellspacing="0" cellpadding="0" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left"> 
+                                <tr style="border-collapse:collapse"> 
+                                <td class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;width:369px"> 
+                                <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                    <tr style="border-collapse:collapse"> 
+                                    <td align="center" bgcolor="#ffffff" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:24px;color:#333333;font-size:16px"><strong>Váš RealQuik Report Tracker</strong></p></td> 
+                                    </tr> 
+                                </table></td> 
+                                </tr> 
+                            </table>
+
+
+                            <table cellspacing="0" cellpadding="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                            <tr style="border-collapse:collapse"> 
+                            <td align="left" style="padding:0;Margin:0;width:191px"> 
+                                <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                <tr style="border-collapse:collapse"> 
+                                <td align="center" bgcolor="#ffffff" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Pravidelný report ve 13:05</p></td> 
+                                </tr> 
+                                </table></td> 
+                            </tr> 
+                            </table> 
+                         </td> 
+                       </tr> 
+                   </table> 
+                   <table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%"> 
+                    <tr style="border-collapse:collapse"> 
+                     <td align="center" style="padding:0;Margin:0"> 
+                      <table class="es-content-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;width:600px" cellspacing="0" cellpadding="0" align="center" bgcolor="#333333"> 
+                        <tr style="border-collapse:collapse"> 
+                         <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;background-color:#FFFFFF"> 
+                          <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                            <tr style="border-collapse:collapse"> 
+                             <td align="center" valign="top" style="padding:0;Margin:0;width:560px"> 
+                              <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                <tr style="border-collapse:collapse"> 
+                                 <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Dobrý den,</p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">na základě vašeho hlídacího psa <strong>{tracker_id}</strong> vám posíláme následující byty ve vámi zadané lokalitě <strong>{district}</strong>, které <strong>{today}</strong> náš systém vyhodnotil jako pravděpodobně podceněné.</p></td> 
+                                </tr> 
+                              </table></td> 
+                            </tr> 
+                          </table></td> 
+                        </tr> 
+                        <tr style="border-collapse:collapse"> 
+                         <td align="center" bgcolor="#ffffff" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;background-color:#FFFFFF"> 
+                          <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                            <tr style="border-collapse:collapse"> 
+                             <td align="center" valign="top" style="padding:0;Margin:0;width:560px"> 
+                              <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                <tr style="border-collapse:collapse"> 
+                                 <td align="center" style="padding:0;Margin:0"  style="border-style:solid;border-color:#ffffff;background:#2CB543;border-width:0px;display:inline-block;border-radius:15px;width:auto"><a  style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;box-shadow: 1px 1px 4px black;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#000000;  font-size:18px;border-style:solid;border-color:#ffffff;border-width:10px 20px 10px 20px;display:inline-block;background:#ffffff;border-radius:15px;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-weight:normal;font-style:normal;line-height:22px;width:auto;text-align:center;"> 
+                                    <ul>{links_joined}
+                                    </ul></td> 
+                                </tr> 
+                              </table></td> 
+                            </tr> 
+                          </table></td> 
+                        </tr> 
+                        <tr style="border-collapse:collapse"> 
+                         <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;background-color:#FFFFFF"> 
+                          <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                            <tr style="border-collapse:collapse"> 
+                             <td align="center" valign="top" style="padding:0;Margin:0;width:560px"> 
+                              <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                <tr style="border-collapse:collapse"> 
+                                 <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Hezký zbytek dne přeje,</p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px"><strong>RealQuik a.s.</strong></p></td> 
+                                </tr> 
+                              </table></td> 
+                            </tr> 
+                          </table></td> 
+                        </tr> 
+                        <tr style="border-collapse:collapse"> 
+                         <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;background-color:#FFFFFF"> 
+                          <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                            <tr style="border-collapse:collapse"> 
+                             <td align="center" valign="top" style="padding:0;Margin:0;width:560px"> 
+                              <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
+                                <tr style="border-collapse:collapse"> 
+                                 <td align="center" style="padding:0;Margin:0;font-size:0px"><img class="adapt-img" src="https://lolofb.stripocdn.email/content/guids/50063606-1cf0-4b63-b046-265c7f7f4a6b/images/48381621333814262.jpg" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="350"></td> 
+                                </tr> 
+                              </table></td> 
+                            </tr> 
+                          </table></td> 
+                        </tr> 
+                      </table></td> 
+                    </tr> 
+                  </table>
+                </td> 
+            </tr> 
+          </table> 
+                </div>
+
                 </body>
+
             </html>
             """
 
